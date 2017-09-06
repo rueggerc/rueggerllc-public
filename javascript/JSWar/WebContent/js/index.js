f3430d8aff4d9be7ea8a2e22f4a9bc0c3e620163
Chris Ruegger <chris.ruegger@gmail.com>
2017-09-06 07:46:36 -0400

    JSWar index.js

--- javascript/JSWar/WebContent/js/index.js
+++ javascript/JSWar/WebContent/js/index.js
@@ -6,7 +6,19 @@ $(document).ready(function() {
 
 var myCar = {
 	make: "inifinit",
-	model: "g37x"
+	model: "g37x",
+	drive: function(speed) {
+		console.log("Drive my " + this.make + " at MPH=" + speed);
+	},
+	cruise: function(speed) {
+		console.log("CRUISE BEGIN");
+		var that = this;
+		var helper = function() {
+			console.log("I help the " + that.make + " cruise");
+		}
+		helper();
+	}
+	
 };
 
 var adder = function(x) {
@@ -15,9 +27,31 @@ var adder = function(x) {
 	};
 };
 
+var foobar = function(x,y) {
+	var z = x + y;
+	console.log("foobar result=" + z);
+	console.log("foobar this=" + this);
+	console.log("arguments=" + arguments);
+	console.log("arguments size=" + arguments.length);
+	for (name in this) {
+		// console.log("next this=" + name);
+	}
+}
+
+
+function myAdd(a,b,c,d) {
+	console.log("myAdd a=" + a);
+	console.log("myAdd b=" + b);
+	console.log("myAdd c=" + c);
+	console.log("myAdd d=" + d);
+}
+
 
 function setPageState() {
 	console.log("Set Page State BEGIN");
+	foobar(3,9);
+	
+	
 	const x = 42;
 	let y = 33;
 	for (let i=0; i < 10; i++) {
@@ -44,6 +78,21 @@ function setPageState() {
 	add5 = adder(5);
 	console.log("adder5=" + add5(3));
 	
+	// Method Invocation Pattern
+	myCar.drive(60);
+	
+	// Function Invocation Pattern
+	myCar.cruise(55);
+	
+	// Constructor Invocation Pattern
+	
+	
+	// Apply Invocation Pattern
+	var myList = [3,4];
+	var myListSum = myAdd.apply(null,myList);
+	console.log("myListSum=" + myListSum);
+	
+	console.log("Dump Car Properties BEGIN");
 	for (name in myCar) {
 		console.log("next car property:" + name);
 	}
