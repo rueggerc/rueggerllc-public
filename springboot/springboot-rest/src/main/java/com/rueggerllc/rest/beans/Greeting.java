a86950de18c1d5ec23aaf1051268f297b4bc4f78
Chris <chris.ruegger@gmail.com>
2017-10-10 18:14:30 -0400

    Added application.properties

--- springboot/springboot-rest/src/main/java/com/rueggerllc/rest/beans/Greeting.java
+++ springboot/springboot-rest/src/main/java/com/rueggerllc/rest/beans/Greeting.java
@@ -17,4 +17,6 @@ public class Greeting {
     public String getContent() {
         return content;
     }
+    
+    
 }
--- springboot/springboot-rest/src/main/java/com/rueggerllc/rest/controllers/GreetingController.java
+++ springboot/springboot-rest/src/main/java/com/rueggerllc/rest/controllers/GreetingController.java
@@ -1,9 +1,13 @@
 package com.rueggerllc.rest.controllers;
 
 import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 import com.rueggerllc.rest.beans.Greeting;
 
 @RestController
@@ -12,9 +16,29 @@ public class GreetingController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
+//    @RequestMapping(value="/foobar",
+//    		        method=RequestMethod.GET,
+//    		        produces={MediaType.APPLICATION_JSON_VALUE,MediaType.APPLICATION_XML_VALUE})
+//    public Greeting foobar(@RequestParam(value="name", defaultValue="World") String name) {
+//        System.out.println("GreetingController.foobar()");
+//    	return new Greeting(counter.incrementAndGet(),String.format(template, name));
+//    }
+
+	@RequestMapping(value = "/foobar", 
+			        method = RequestMethod.GET)
+	public Greeting foobar(@RequestParam(value = "name", defaultValue = "World") String name) {
+		System.out.println("GreetingController.foobar()");
+		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
+    
+
+    
+    
+    
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-        return new Greeting(counter.incrementAndGet(),
-                            String.format(template, name));
+    	System.out.println("GreetingController.greeting()");
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
+    
 }
